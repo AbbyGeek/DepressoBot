4f4bf376f4ed848165c25cc7652f69c62d6950f1
West, Abby <abbyrosewest@gmail.com>
2019-04-15 14:12:04 -0400

    Add project files.

---
+++ DepressoBot/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TweetSharp;
+using System.Timers;
+
+
+namespace DepressoBot
+{
+    class Program
+    {
+
+        private static Timer aTimer;
+        static void Main()
+        {
+
+
+            SetTimer();
+
+            Console.WriteLine("\nPress the Enter key to exit the application...\n");
+            Console.WriteLine("The application started at {0:HH:mm:ss.fff}", DateTime.Now);
+            string tweet = Generator.GenerateTweet();
+            Tweeter.tweeter(tweet);
+            Console.ReadLine();
+            aTimer.Stop();
+            aTimer.Dispose();
+            Console.WriteLine("Terminating the application...");
+
+
+        }
+
+        private static void SetTimer()
+        {
+            //create timer with 1 min interval
+            aTimer = new Timer(60000 * 60);
+            //Set event to occur when time has elapsed
+            aTimer.Elapsed += OnTimedEvent;
+            aTimer.AutoReset = true;
+            aTimer.Enabled = true;
+
+        }
+
+
+        private static void OnTimedEvent(Object source, ElapsedEventArgs e)
+        {
+            string tweet = Generator.GenerateTweet();
+            Tweeter.tweeter(tweet);
+        }
+
+
+
+    }
+}
+
---
+++ DepressoBot/Tweeter.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TweetSharp;
+using System.IO;
+
+namespace DepressoBot
+{
+    class Tweeter
+    {
+        public static void tweeter(string tweet)
+        {
+            TwitterService service = authenticate();
+            service.SendTweet(new SendTweetOptions { Status = tweet }, (Tweet, response) =>
+            {
+                if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine(tweet);
+                    Console.WriteLine($"{DateTime.Now} - TWEET SENT!");
+                    Console.ResetColor();
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(tweet);
+                    Console.WriteLine($"{DateTime.Now} = TWEET NOT SENT");
+                    Console.ResetColor();
+                }
+            });
+        }
+
+
+
+
+        private static TwitterService authenticate()
+        {
+            string[] lines = File.ReadAllLines("settings.txt");
+            string _customerKey = lines[0];
+            string _customerSecret = lines[1];
+            string _accessToken = lines[2];
+            string _accessSecret = lines[3];
+
+
+            TwitterService service = new TwitterService(_customerKey, _customerSecret, _accessToken, _accessSecret);
+
+            return service;
+        }
+    }
+}
